5ffb51876f515e50dab851eb90b99d7369840921
Bad-Karmaa <caoviethoang.lfay@gmail.com>
2021-12-29 16:27:50 +0100

    rajouter la fonction moveby1 dans la classe hero pour le LL

--- squelette/src/gameWorld/TESTROOM.java
+++ squelette/src/gameWorld/TESTROOM.java
@@ -1,28 +1,38 @@
 package gameWorld;
 
+import java.util.Collection;
 import java.util.LinkedList;
 
+import gameobjects.Fly;
 import gameobjects.Hero;
 import gameobjects.Spider;
 import resources.CycleInfos;
 import resources.ImagePaths;
 import resources.RoomInfos;
 import resources.SpiderInfos;
+import resources.FlyInfos;
+
 
 //classe de test pour tes appels de monstres ;p
 
 public class TESTROOM extends SpawnRoom {
 	
-	private LinkedList<Spider> monsters;
+	private LinkedList<Hero> monsters;
 
 	public TESTROOM(Hero hero) {
 		super(hero);
-		this.monsters = new LinkedList<Spider>();
+		this.monsters = new LinkedList<Hero>();
 		Spider spider1=new Spider(RoomInfos.POSITION_CENTER_OF_ROOM, SpiderInfos.SPIDER_SIZE, SpiderInfos.SPIDER_SPEED, ImagePaths.SPIDER);
 		addmonster(spider1);
+		Fly fly1= new Fly(RoomInfos.POSITION_CENTER_OF_ROOM, FlyInfos.FLY_SIZE, FlyInfos.FLY_SPEED, ImagePaths.FLY);
+		addmonster(fly1);
 		// TODO Auto-generated constructor stub
 	}
 	
+	private void addmonster(Fly fly1) {
+		this.monsters.add(fly1);
+	}
+
 	public void addmonster(Spider monster) {
 		this.monsters.add(monster);
 	}
@@ -33,15 +43,18 @@ public class TESTROOM extends SpawnRoom {
 		}
 	}
 	public void moveby1allMonsters() {
-		for (Spider m: this.monsters) {
-			m.moveby1();
+		
+		for (Hero m: this.monsters) { m.moveby1(); }
+		
+		for (Hero n: this.monsters) {
+			n.moveby1();
 		}
 	}
 	
 	@Override
 	public void drawRoom() {
 		super.drawRoom();
-		if (CycleInfos.Cycle%2==0)
+		if (CycleInfos.Cycle%3==0)
 		moveby1allMonsters();
 		drawmonsters();
 	}
--- squelette/src/gameobjects/Fly.java
+++ squelette/src/gameobjects/Fly.java
@@ -5,6 +5,39 @@ import libraries.Vector2;
 import java.util.Random;
 
 public class Fly extends Hero{
-	public Fly 
+	public Fly(Vector2 position, Vector2 size, double speed, String imagepath) {
+		super(position, size, speed, imagepath);
+	}
+	
+	public void updateObjects() {
+		move();
+	}
+	
+	private void move() {
+		Vector2 normalizedDirection = getNormalizedDirection();
+		Vector2 positionAfterMoving = getPosition().addVector(normalizedDirection);
+		setPosition(positionAfterMoving);
+		getDirection();
+		}
+	
+	
+	public void moveby1() {
+		double b= getrandomdouble(3)-1;
+		double a= getrandomdouble(3)-1;
+		getDirection().addX(a);
+		getDirection().addY(b);
+		move();
+	}
+	
+	
+	public double getrandomdouble(int max) {
+		return Math.floor(Math.random()*max);
+	}
+	
+	public void drawGameObject()
+	{
+		StdDraw.picture(getPosition().getX(), getPosition().getY(), getImagePath(), getSize().getX(), getSize().getY(),
+				0);}
 
 }
+
