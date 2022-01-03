1ec70aca140cd31a7287af569e77304614e0890d
ExWorlds <exshadowar@gmail.com>
2022-01-03 19:32:29 +0100

    Update GameWorld.java
    started implementing  auto-generation dungeon

--- squelette/src/gameWorld/GameWorld.java
+++ squelette/src/gameWorld/GameWorld.java
@@ -3,17 +3,96 @@ package gameWorld;
 import gameobjects.Hero;
 import libraries.StdDraw;
 import resources.Controls;
+import java.util.Random;
 
 public class GameWorld
 {
 	private Room currentRoom;
 	private Hero hero;
+	private Room[][] Donjon;
+	private int LV;
 
 	// A world needs a hero
 	public GameWorld(Hero hero)
 	{
 		this.hero = hero;
 		currentRoom = new Room(hero);
+		this.Donjon = new Room[4][4];
+		this.LV = 1;
+	}
+	
+	public void createDungeon() {
+		int numberOfRoom = giveNumberOfRoom(this.LV);
+		int count =0;
+		Random rand = new Random();
+		boolean[][] generationDJ = new boolean[4][4];
+		int x = rand.nextInt(4);
+		int y = rand.nextInt(4);
+		generationDJ[y][x] = true;
+		count ++;
+		checkAroundandAdd(generationDJ, x, y, count, numberOfRoom);
+	}
+	
+	/**
+	 * cree les 1 autour du premier 1
+	 * @param genTab
+	 * @param count
+	 * @param nbR
+	 * @return
+	 */
+	public void checkAroundandAdd(boolean[][] genTab, int x, int y, int count, int nbR ) {
+		Random rand = new Random();
+		boolean jobdone = false;
+		if(!(count==nbR)) {
+			while(!jobdone) {
+				int ran = rand.nextInt(4);
+				switch (ran) {
+				case 1 : 
+					if(x!=0 && !genTab[y][x-1]) {
+						genTab[y][x-1] = true;
+						jobdone = true;
+						checkAroundandAdd(genTab, x-1, y, count+1, nbR);
+					}
+					break;
+				case 2 :
+					if(x!=genTab[y].length-1 && !genTab[y][x+1]) {
+						genTab[y][x+1] = true;
+						jobdone = true;
+						checkAroundandAdd(genTab, x+1, y, count+1, nbR);
+					}
+					break;
+				case 3 :
+					if(y!=0 && !genTab[y-1][x]) {
+						genTab[y-1][x] = true;
+						jobdone = true;
+						checkAroundandAdd(genTab, x, y-1, count+1, nbR);
+					}
+					break;
+				default :
+					if(y!=genTab.length-1 && !genTab[y+1][x]) {
+						genTab[y+1][x] = true;
+						jobdone = true;
+						checkAroundandAdd(genTab, x, y+1, count+1, nbR);
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * donne le nombre de room par rapport au LV
+	 * @param LV le niveau du DJ
+	 * @return le nombre de room correspondant
+	 */
+	public int giveNumberOfRoom(int LV) {
+		switch(LV) {
+		case 1: return 5;
+		case 2: return 6;
+		case 3: return 7;
+		case 4: return 8;
+		case 5: return 9;
+		default : return -1;
+		}
 	}
 
 	public void processUserInput()
